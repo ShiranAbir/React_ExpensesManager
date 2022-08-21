f54d3727923a59543d1ddf403e8a52bf562b8646
Shiran Abir <shirkiabir@gmail.com>
2022-08-21 15:20:43 +0300

    Added more items.

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -4,6 +4,9 @@ import './Expenses.css'
 function Expenses({items}){
     return <div className="expenses">
         <ExpenseItem title={items[0].title} date={items[0].date} amount={items[0].amount}/>
+        <ExpenseItem title={items[1].title} date={items[1].date} amount={items[1].amount}/>
+        <ExpenseItem title={items[2].title} date={items[2].date} amount={items[2].amount}/>
+        <ExpenseItem title={items[3].title} date={items[3].date} amount={items[3].amount}/>
     </div>
 }
 
