e9f9217ed4e31efea2dcba802e9424eb642b4162
Shiran Abir <shirkiabir@gmail.com>
2022-08-25 11:42:12 +0300

    Support clear form after submit

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -2,32 +2,37 @@ import React, {useState} from "react"
 import './ExpenseForm.css'
 
 function ExpenseForm(){
-
-    const [userInput, setUserInput] = useState({
-        enteredTitle: '',
-        enteredAmount: '',
-        enteredDate: '',
-    })
+    const [enteredTitle, setEnteredTitle] = useState('')
+    const [enteredAmount, setEnteredAmount] = useState('')
+    const [enteredDate, setEnteredDate] = useState('')
+    // const [userInput, setUserInput] = useState({
+    //     enteredTitle: '',
+    //     enteredAmount: '',
+    //     enteredDate: ''
+    // })
 
     const titleChangeHandler = (event) => {
-        setUserInput({
-            ...userInput,
-            enteredTitle: event.target.value,
-        })
+        setEnteredTitle(event.target.value)
+        // setUserInput({
+        //     ...userInput,
+        //     enteredTitle: event.target.value,
+        // })
     }
 
     const amountChangeHandler = (event) => {
-        setUserInput({
-            ...userInput,
-            enteredAmount: event.target.value,
-        })
+        setEnteredAmount(event.target.value)
+        // setUserInput({
+        //     ...userInput,
+        //     enteredAmount: event.target.value,
+        // })
     }
 
     const dateChangeHandler = (event) => {
-        setUserInput({
-            ...userInput,
-            enteredDate: event.target.value,
-        })
+        setEnteredDate(event.target.value)
+        // setUserInput({
+        //     ...userInput,
+        //     enteredDate: event.target.value,
+        // })
     }
 
     const submitHandler = (event) =>{
@@ -38,21 +43,42 @@ function ExpenseForm(){
             amount: enteredAmount,
             date: new Date(enteredDate)
         }
+
+        setEnteredTitle('')
+        setEnteredAmount('')
+        setEnteredDate('')
+
     }
-ד
+
     return <form onSubmit={submitHandler}>
         <div className="new-expense__controls">
         <div className="new-expense__control">
             <label>Title</label>
-            <input type="text" value={enteredTitle} onChange={titleChangeHandler}/>
+            <input 
+                type="text" 
+                value={enteredTitle} 
+                onChange={titleChangeHandler}
+            />
         </div>
         <div className="new-expense__control">
             <label>Amount</label>
-            <input type="number" min="0.01" step="0.01" onChange={amountChangeHandler}/>
+            <input 
+                type="number" 
+                min="0.01" 
+                step="0.01" 
+                value={enteredAmount}
+                onChange={amountChangeHandler}
+            />
         </div>
         <div className="new-expense__control">
             <label>Date</label>
-            <input type="date" min="2019-01-01" max="2022-12-31" onChange={dateChangeHandler}/>
+            <input 
+                type="date" 
+                min="2019-01-01" 
+                max="2022-12-31" 
+                value={enteredDate}
+                onChange={dateChangeHandler}
+            />
         </div>
         </div>
         <div className="new-expense__actions">
