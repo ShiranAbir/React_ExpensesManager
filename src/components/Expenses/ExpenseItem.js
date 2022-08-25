035e5018b9ab473cd1b29b78132d57995049bf19
Shiran Abir <shirkiabir@gmail.com>
2022-08-25 17:11:54 +0300

    Delete button.

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,22 +1,16 @@
-import React, {useState} from 'react'
+import React from 'react'
 import './ExpenseItem.css'
 import ExpenseDate from './ExpenseDate'
 import Card from '../UI/Card'
 
 function ExpenseItem({title,amount,date}) {
-    const [itemTitle,setTitle] = useState(title)
-    const clickHandler = () => {
-        setTitle('Updated!')
-    }
-
-    return( 
+return( 
     <Card className='expense-item'>
         <ExpenseDate date={date}/>
         <div className='expense-item__description'>
-            <h2>{itemTitle}</h2>
+            <h2>{title}</h2>
             <div className='expense-item__price'>${amount}</div>
         </div>
-        <button onClick={clickHandler}>Change title</button>
     </Card>)
 } 
 
