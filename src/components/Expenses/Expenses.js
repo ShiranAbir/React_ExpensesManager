8f55f6cfa710d49370bb941ba35bb816e4e88c8f
Shiran Abir <shirkiabir@gmail.com>
2022-08-26 16:48:52 +0300

    Show message when 0 expense.

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -12,16 +12,25 @@ function Expenses({items}){
     const filteredExpenses = items.filter(expense=>{
         return expense.date.getFullYear().toString() === filteredYear
     })
+
+    let expensesContent = <p>No expenses found!</p>
+
+    if (filteredExpenses.length) {
+        expensesContent = filteredExpenses.map(expense => (
+            <ExpenseItem
+              key={expense.id}   
+              title={expense.title} 
+              amount={expense.amount} 
+              date={expense.date}
+           />
+        ))
+    }
     return <Card className="expenses">
-            <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler}/>
-            {filteredExpenses.map(expense => (
-                <ExpenseItem
-                  key={expense.id}   
-                  title={expense.title} 
-                  amount={expense.amount} 
-                  date={expense.date}
-               />
-            ))}
+            <ExpensesFilter 
+                selected={filteredYear} 
+                onChangeFilter={filterChangeHandler}
+            />
+            {expensesContent}
         </Card>
 }
 
