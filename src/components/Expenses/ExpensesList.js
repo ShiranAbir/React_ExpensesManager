abe08eb7ed47b94a466388e73b92c88eaa11e11e
Shiran Abir <shirkiabir@gmail.com>
2022-10-11 10:30:49 +0300

    Create ExpensesList component.

--- src/components/Expenses/ExpensesList.js
+++ src/components/Expenses/ExpensesList.js
@@ -1,20 +1,22 @@
 import React from 'react'
 
 import ExpenseItem from './ExpenseItem'
+import './ExpensesList.css'
 
 const ExpenseList = props => {
-    
-    let expensesContent = <p>No expenses found!</p>
 
-    if (props.filteredExpenses.length) {
-        expensesContent = props.filteredExpenses.map(expense => (
+    if (!props.items.length){
+        return <h2 className='expenses-list__fallback'>Found no expenses!</h2>
+    }
+    return <ul className='expenses-list'>
+        {props.items.map(expense => (
             <ExpenseItem
               key={expense.id}   
               title={expense.title} 
               amount={expense.amount} 
               date={expense.date}
            />
-        ))
-    }
+        ))}
+    </ul>
 }
 export default ExpenseList
